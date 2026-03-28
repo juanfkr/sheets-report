0dda94d0734fb24ebb4f833f9385d412e07c44c0
juanfkr <juanferreiraverao@gmail.com>
2026-03-28 10:44:06 -0400

    feat: seleção especifica de colunas por planilha

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from services.pdf_generator_service import PdfGeneratorService
+from services.dataframe_service import DataFrameService
 from core.path_handler import PathHandler
-from services.csv_converter_service import CsvConverterService
 
 path_handler = PathHandler()
 
@@ -12,9 +12,9 @@ pdf_generator = PdfGeneratorService(f"{PDFS_DIRECTORY}/teste.pdf")
 
 pdf_generator.generate_pdf("teste")
 
-csv_converter = CsvConverterService()
+df = DataFrameService()
+df.csv_to_dataframe(f"{SHEETS_DIRECTORY}/Amazon-sales.csv")
 
-csv_converter.csv_to_dataframe(f"{SHEETS_DIRECTORY}/Amazon-sales.csv")
-df = csv_converter.get_dataframe()
+local_df = df.get_dataframe()
 
-print(df)
+print(local_df)
--- src/services/dataframe_service.py
+++ src/services/dataframe_service.py
@@ -1,13 +1,34 @@
-from pandas import DataFrame
+from pandas import read_csv, DataFrame
 
 class DataFrameService:
-    def __init__(self, data_frame: DataFrame):
-        self.data_frame = data_frame
+    def __init__(self):
+        self.dataframe = None
 
     # TODO: novo dataframe com dados tratados a partir do DF populacionado
     # deve incluir:
-    # OrderID | OrderDate | CustomerName | ProductID | ProductName | Category | Brand | Quantity
+    # OrderID | OrderDate | ProductID | ProductName | Category | Brand | Quantity
     # UnitPrice | Discount | Tax | ShippingCost | TotalAmount | PaymentMethod | OrderStatus |
     # City | State | Country
-    def sanitized_dataframe(self):
-        pass
+    def csv_to_dataframe(self, csv: str) -> None:
+        self.dataframe = read_csv(csv, usecols=[
+            "OrderID",
+            "OrderDate",
+            "ProductID",
+            "ProductName",
+            "Category",
+            "Brand",
+            "Quantity",
+            "UnitPrice",
+            "Discount",
+            "Tax",
+            "ShippingCost",
+            "TotalAmount",
+            "PaymentMethod",
+            "OrderStatus",
+            "City",
+            "State",
+            "Country"
+        ])
+
+    def get_dataframe(self) -> DataFrame:
+        return self.dataframe
