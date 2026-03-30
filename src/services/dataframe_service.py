61b32d3b6e4d65e5a7450be922b3b10c0452b5f8
juanfkr <juanferreiraverao@gmail.com>
2026-03-30 11:02:52 -0400

    feat: leitura e exibição do dataframe

--- src/services/dataframe_service.py
+++ src/services/dataframe_service.py
@@ -1,14 +1,14 @@
+from datetime import datetime
+
 from pandas import read_csv, DataFrame
 
 class DataFrameService:
     def __init__(self):
         self.dataframe = None
 
-    # TODO: novo dataframe com dados tratados a partir do DF populacionado
-    # deve incluir:
-    # OrderID | OrderDate | ProductID | ProductName | Category | Brand | Quantity
-    # UnitPrice | Discount | Tax | ShippingCost | TotalAmount | PaymentMethod | OrderStatus |
-    # City | State | Country
+    def get_dataframe(self) -> DataFrame:
+        return self.dataframe
+
     def csv_to_dataframe(self, csv: str) -> None:
         self.dataframe = read_csv(csv, usecols=[
             "OrderID",
@@ -29,6 +29,3 @@ class DataFrameService:
             "State",
             "Country"
         ])
-
-    def get_dataframe(self) -> DataFrame:
-        return self.dataframe
