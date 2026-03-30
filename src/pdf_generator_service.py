01ee3283699ef16b127bb8b17dc7dee40530fce8
juanfkr <juanferreiraverao@gmail.com>
2026-03-30 11:31:14 -0400

    refactor: remoção temporária de PdfGeneratorService

--- src/pdf_generator_service.py
+++
@@ -1,8 +0,0 @@
-from weasyprint import HTML
-
-class PdfGeneratorService:
-    def __init__(self, target: str) -> None:
-        self.target = target
-
-    def generate_pdf(self, htmlStructure: str) -> None:
-        HTML(string=f"<p>{htmlStructure}</p>").write_pdf(self.target, pdf_variant="pdf/a-3u")
